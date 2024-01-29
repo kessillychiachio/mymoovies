2ae11b87b7e6a4756d2320a1f1d1e5e36b725a67
kessillychiachio <kessilly_chiachio@hotmail.com>
2024-01-28 22:27:04 -0300

    Criacao dos metodos de categoria

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const CategoryController = require("../controllers/CategoryController.js");
 
-router.get("/", function (req,res) {
-  res.send("Entrei em Category"); 
-});
+router.get("/", CategoryController.findAll);
+router.get("/:id", CategoryController.find);
+router.post("/", CategoryController.create);
+router.delete("/:id", CategoryController.delete);
 
 module.exports = router;
